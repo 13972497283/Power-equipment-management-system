22c703c341f347f861f77c9b6c4a9dcdbb9c610e
username <1542090746@qq.com>
2022-03-24 17:02:03 +0800

    报废功能

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,33 @@
 import { createStore } from 'vuex'
+const getCurrentTime = () => {
+  var date = new Date();
+  var sign1 = "-";
+  var sign2 = ":";
+  var year = date.getFullYear() // 年
+  var month = date.getMonth() + 1; // 月
+  var day = date.getDate(); // 日
+  var hour = date.getHours(); // 时
+  var minutes = date.getMinutes(); // 分
+  var seconds = date.getSeconds() //秒
+  if (month >= 1 && month <= 9) {
+    month = "0" + month;
+  }
+  if (day >= 0 && day <= 9) {
+    day = "0" + day;
+  }
+  if (hour >= 0 && hour <= 9) {
+    hour = "0" + hour;
+  }
+  if (minutes >= 0 && minutes <= 9) {
+    minutes = "0" + minutes;
+  }
+  if (seconds >= 0 && seconds <= 9) {
+    seconds = "0" + seconds;
+  }
+  var currentTime = year + sign1 + month + sign1 + day + " " + hour + sign2 + minutes + sign2 + seconds
 
+  return currentTime
+}
 export default createStore({
   state: {
     identifications: [{
@@ -10,13 +38,56 @@ export default createStore({
       name: 'gLeader',
       password: '123456'
     }], //管理员与组长的账号密码
-    loginState: null//判断登陆者的身份，1为管理员，2为组长,0是未登录
+    loginState: null,//判断登陆者的身份，1为管理员，2为组长,0是未登录
+    equipmentList: [{
+      RFID: '001',
+      status: '正常',//运行状态
+      manufacturer: '三峡电力设备有限公司',//厂家
+      money: '180',
+      buyTime: '2022-2-19',//购置时间
+      scrapTime: '',//报废时间
+      location: '湖北宜昌',
+      use: '用于三峡大学教学楼供电',//用途
+      type: '配电柜',
+      maintainList: [{ time: '2022-3-19', describe: '外壳维修' }, { time: '2022-3-19', describe: '外壳维修' }],//维修情况
+    }, {
+      RFID: '002',
+      status: '正常',//运行状态
+      maintainList: [{ time: '2022-3-19', describe: '外壳维修' }, { time: '2022-3-19', describe: '外壳维修' }],//维修情况
+      manufacturer: '三峡电力设备有限公司',//厂家
+      money: '180',
+      buyTime: '2022-2-19',//购置时间
+      scrapTime: '',//报废时间
+      location: '湖北宜昌',
+      use: '用于三峡大学教学楼供电',//用途
+      type: '配电柜'
+    },],
+
+    historyRecord: [{
+      time: '',
+      RFID: '',
+      handle: '',//操作
+      handler: ''//操作者
+    }]
+
   },
   mutations: {
+
+
     setLoginState (state, payload) {//payload为参数
       state.loginState = payload
+    },
+    // 添加设备
+    addEquipments (state, payload) {
+      state.equipmentList.push(payload)
+    },
+    scrapEquipment (state, payload) {
+      state.equipmentList[payload].status = "已报废"
+      state.equipmentList[payload].scrapTime = getCurrentTime()
     }
   },
+
+
   actions: {
   },
   modules: {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -22,15 +22,105 @@
         <el-divider style="margin:0px" />
         <div class="menu">台账查询</div>
       </div>
+
       <div class="continer__main__content">
-        <el-table :data="tableData" style="width: 9.99rem" height="425" border highlight-current-row fit>
-          <el-table-column fixed prop="date" label="Date" width="150" />
-          <el-table-column prop="name" label="Name" width="120" />
-          <el-table-column prop="state" label="State" width="120" />
-          <el-table-column prop="city" label="City" width="320" />
-          <el-table-column prop="address" label="Address" width="600" />
-          <el-table-column prop="zip" label="Zip" width="120" fixed="right" />
+        <div class="buttons">
+          <el-input placeholder="请输入关键字" clearable v-model="input" style="width:3rem ;margin-right:.1rem" />
+          <el-button type="primary"><span class="iconfont" style="margin-right:.05rem;font-size:.2rem">&#xe621;</span>查询
+          </el-button>
+          <el-button type="primary" @click="handleAdd"><span class="iconfont"
+              style="margin-right:.05rem">&#xe60e;</span>新增
+          </el-button>
+        </div>
+
+
+        <el-table :data="tableData" style="width: 9.8rem" height="395" border highlight-current-row fit>
+          <el-table-column fixed prop="RFID" label="RFID编码" align="center" width="100" />
+          <el-table-column prop="status" label="运行状态" align="center" width="100" />
+          <el-table-column prop="manufacturer" label="厂家" align="center" width="200" />
+          <el-table-column prop="money" label="购置金额" align="center" width="100" />
+          <el-table-column prop="buyTime" label="购置时间" align="center" width="200" />
+          <el-table-column prop="scrapTime" label="报废时间" align="center" width="200" />
+          <el-table-column prop="location" label="所在地" align="center" width="200" />
+          <el-table-column prop="use" label="设备用途" align="center" width="200" />
+          <el-table-column prop="type" label="设备类型" align="center" width="100" />
+          <!-- prop="maintainList"  scope.row就相当于取这一行的所有数据，即列表中这完整的一项-->
+          <el-table-column label="维修情况" align="center" width="200">
+            <template v-slot="scope">
+              <div v-for="(item,index) in scope.row.maintainList" :key="index">
+                {{item.time}}:{{item.describe}}
+              </div>
+            </template>
+          </el-table-column>
+
+
+          <el-table-column label=" 操作" fixed="right" align="center" width="200">
+            <!-- <div> -->
+            <template v-slot="scope">
+              <el-button @click="openDetail(scope)">查看</el-button>
+              <el-button type="primary" :disabled="notUse(scope.$index,scope.row)">编辑</el-button>
+              <el-button type="danger" :disabled="notUse(scope.$index,scope.row)" @click="scrapItem(scope.$index)">报废
+              </el-button>
+            </template>
+
+          </el-table-column>
         </el-table>
+
+
+        <el-dialog v-model="dialogVisible" title="添加" width="60%" :before-close="handleClose">
+          <!-- 添加设备 -->
+          <el-form :model="form" label-width="120px" :rules="rules" ref="formrule">
+            <!--  ref用于将表单节点绑定在此元素上 -->
+            <div style="display:flex">
+              <el-form-item label="RFID编码" prop="RFID">
+                <el-input v-model="form.RFID" style="width:2rem " />
+              </el-form-item>
+              <el-form-item label="设备用途" prop="use">
+                <el-input style="width:2rem" v-model="form.use" />
+              </el-form-item>
+            </div>
+            <div style="display:flex">
+              <el-form-item label="设备类型" prop="type">
+                <el-input style="width:2rem" v-model="form.type" />
+              </el-form-item>
+              <el-form-item label="所在地" prop="location">
+                <el-input style="width:2rem" v-model="form.location" />
+              </el-form-item>
+            </div>
+            <div style="display:flex">
+              <el-form-item label="购置时间" prop="buyTime">
+                <el-date-picker v-model="form.buyTime" type="date" placeholder="选择日期" style="width: 2rem"
+                  format="YYYY/MM/DD" value-format="YYYY-MM-DD" />
+              </el-form-item>
+              <el-form-item label="购置金额" prop="money">
+                <el-input style="width:2rem" v-model="form.money" placeholder="单位：元" />
+              </el-form-item>
+            </div>
+            <div style="display:flex">
+              <el-form-item label="厂家" prop="manufacturer">
+                <el-input style="width:2rem" v-model="form.manufacturer" />
+              </el-form-item>
+              <el-form-item label="运行状态" prop="status">
+                <el-select v-model="form.status" placeholder="选择状态" style="width:2rem">
+                  <el-option label="正常" value="正常" />
+                  <el-option label="暂停" value="暂停" />
+                  <el-option label="维修中" value="维修中" />
+                  <!-- <el-option label="报废" value="报废" /> -->
+                </el-select>
+              </el-form-item>
+              <!-- <input v-model="test" /> -->
+            </div>
+          </el-form>
+          <!-- 添加设备 -->
+
+          <template #footer>
+            <span class="dialog-footer">
+              <el-button @click="dialogVisible = false">取消</el-button>
+              <el-button type="primary" @click="addSubmit">提交</el-button>
+            </span>
+          </template>
+        </el-dialog>
+
       </div>
     </div>
   </div>
@@ -39,73 +129,166 @@
 <script>
   import {
     ref,
-    reactive
+    reactive,
+    nextTick
   } from 'vue'
+  import '../style/iconfont.css'
+  import {
+    ElMessageBox,
+    ElMessage
+  } from 'element-plus'
+  import {
+    useStore
+  } from 'vuex'
+
   export default {
     name: 'Home',
     setup() {
       let showEC = ref(false)
+      let input = ref("")
+      const dialogVisible = ref(false)
+      const store = useStore()
+      const tableData = reactive(store.state.equipmentList)
 
-      const tableData = [{
-          date: '2016-05-03',
-          name: 'Tom',
-          state: 'California',
-          city: 'Los Angeles',
-          address: 'No. 189, Grove St, Los Angeles',
-          zip: 'CA 90036',
-        },
-        {
-          date: '2016-05-02',
-          name: 'Tom',
-          state: 'California',
-          city: 'Los Angeles',
-          address: 'No. 189, Grove St, Los Angeles',
-          zip: 'CA 90036',
-        },
-        {
-          date: '2016-05-04',
-          name: 'Tom',
-          state: 'California',
-          city: 'Los Angeles',
-          address: 'No. 189, Grove St, Los Angeles',
-          zip: 'CA 90036',
-        },
-        {
-          date: '2016-05-01',
-          name: 'Tom',
-          state: 'California',
-          city: 'Los Angeles',
-          address: 'No. 189, Grove St, Los Angeles',
-          zip: 'CA 90036',
-        },
-        {
-          date: '2016-05-08',
-          name: 'Tom',
-          state: 'California',
-          city: 'Los Angeles',
-          address: 'No. 189, Grove St, Los Angeles',
-          zip: 'CA 90036',
-        },
-        {
-          date: '2016-05-06',
-          name: 'Tom',
-          state: 'California',
-          city: 'Los Angeles',
-          address: 'No. 189, Grove St, Los Angeles',
-          zip: 'CA 90036',
-        },
-        {
-          date: '2016-05-07',
-          name: 'Tom',
-          state: 'California',
-          city: 'Los Angeles',
-          address: 'No. 189, Grove St, Los Angeles',
-          zip: 'CA 90036',
+      let form = ref({
+        RFID: '',
+        use: '',
+        type: '',
+        location: '',
+        buyTime: '',
+        money: '',
+        manufacturer: '',
+        status: '',
+      })
+      const handleClose = () => {
+        ElMessageBox.confirm('确定关闭此窗口?')
+          .then(() => {
+            dialogVisible.value = false
+          })
+          .catch(() => {
+            // catch error
+          })
+      }
+      const rules = reactive({
+        RFID: [{
+          required: true,
+          message: '请输入RFID编码',
+          trigger: 'blur'
+        }],
+        use: [{
+          required: true,
+          message: '请输入设备用途',
+          trigger: 'blur',
+        }, ],
+        type: [{
+          required: true,
+          message: '请输入设备类型',
+          trigger: 'blur',
+        }, ],
+        location: [{
+          required: true,
+          message: '请输入所在地',
+          trigger: 'blur',
+        }, ],
+        buyTime: [{
+          type: 'date',
+          required: true,
+          message: '请选择购置时间',
+          trigger: 'blur',
+        }, ],
+        money: [{
+          required: true,
+          message: '请输入购置金额',
+          trigger: 'blur',
+        }, ],
+        manufacturer: [{
+          required: true,
+          message: '请输入厂家',
+          trigger: 'blur'
+        }],
+        status: [{
+          required: true,
+          message: '请选择运行状态',
+          trigger: 'blur'
+        }],
+      })
+      let formrule = ref(null) //将表单节点绑定在此元素上
+
+      const addSubmit = () => {
+
+        const subForm = form.value
+        formrule.value.validate(valid => {
+          if (valid) {
+            console.log(111)
+            store.commit('addEquipments', subForm)
+            console.log(store.state.equipmentList, form)
+            dialogVisible.value = false
+            ElMessage({
+              message: '添加成功',
+              type: 'success',
+            })
+
+          } else {
+
+            ElMessage({
+              message: '添加失败，存在不符合要求的输入',
+              type: 'error',
+            })
+          }
+
+        })
+
+      }
+
+
+
+      const handleAdd = () => {
+        console.log(1)
+        form.value = {
+          RFID: '',
+          use: '',
+          type: '',
+          location: '',
+          buyTime: '',
+          money: '',
+          manufacturer: '',
+          status: '',
         }
-      ]
+        console.log(2)
+        dialogVisible.value = true
+      }
+
+      const scrapItem = (index) => {
+
+        store.commit('scrapEquipment', index)
+
+      }
+      const notUse = (index, row) => {
+        if (row.status == "已报废") {
+          console.log("1")
+          return true
+        }
+        console.log("2")
+        return false
+      }
+
+      const openDetail = (scope) => {
+        console.log(scope, "scope")
+      }
       return {
         tableData,
-        showEC
+        showEC,
+        input,
+        form,
+        dialogVisible,
+        handleClose,
+        rules,
+        addSubmit,
+        formrule,
+        handleAdd,
+        scrapItem,
+        notUse,
+        openDetail
       }
     }
 
@@ -136,6 +319,8 @@
         top: .58rem;
         right: .4rem;
         z-index: 100;
+        cursor: pointer;
+
       }
 
       &__title {
@@ -148,7 +333,9 @@
       }
 
       &__image {
-        margin: auto .65rem
+        margin: auto .65rem;
+        cursor: pointer;
+
       }
     }
 
@@ -183,6 +370,12 @@
     background: #FFF;
     border-radius: .04rem;
     letter-spacing: .02rem;
+    cursor: pointer;
+  }
+
+  .buttons {
+    display: flex;
+    margin-bottom: .1rem;
   }
 
 </style>
